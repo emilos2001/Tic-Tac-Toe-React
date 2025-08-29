83b1d47430a1522a5fd601947a8734c16e7a8153
Emil Bogaci <140043844+emilos2001@users.noreply.github.com>
2025-08-29 19:20:17 +0300

    Update Attributes.jsx
    update

--- TicTacToe/Attributes.jsx
+++ TicTacToe/Attributes.jsx
@@ -1,16 +1,25 @@
 import './style.css'
-import {useState} from 'react'
+import circleSrc from './Assets/circle.png'
+import crossSrc from './Assets/cross.png'
+import {useEffect} from "react";
 
 export function Attributes(props) {
+
     const handleReset = () => {
         props.setBoard(Array(9).fill(''));
         props.setGameOver(prev => !prev);
         props.setIsWinner([])
         props.handleMessages('')
     }
+
     return(
         <>
-            <button className='restart-game' onClick={handleReset}>RESTART</button>
+            <div className='score'>
+                <img src={circleSrc}/>
+                <h1>{props.score0.current}</h1>
+                <img src={crossSrc}/>
+                <h1>{props.scoreX.current}</h1>
+            </div>
+            {props.gameOver && <button className='restart-game' onClick={handleReset}>RESTART</button>}
         </>
-    )
-}
+    )}
