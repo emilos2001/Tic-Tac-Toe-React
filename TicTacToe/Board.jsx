56cc40f86d3644270eca90b9a3a28e913d88681f
Emil Bogaci <140043844+emilos2001@users.noreply.github.com>
2025-08-29 19:21:39 +0300

    Update Board.jsx
    update

--- TicTacToe/Board.jsx
+++ TicTacToe/Board.jsx
@@ -1,14 +1,17 @@
 import './style.css'
 import {Attributes} from './Attributes.jsx'
-import {useState} from 'react'
+import {useState, useRef, useEffect} from 'react'
 import {CalculateWinner} from './CalculateWinner.js'
 import circleSrc from './Assets/circle.png'
 import crossSrc from './Assets/cross.png'
 
 export function Board(props) {
+    const player = useRef(true)
+    const scoreX = useRef(0)
+    const score0 = useRef(0)
+    const turn = useRef(true)
     const [board, setBoard] = useState(Array(9).fill(''))
     const [gameOver, setGameOver] = useState(false)
-    const [xTurn, setXTurn] = useState(true)
     const [isLoser, setIsLoser] = useState(false)
     const [isWinner, setIsWinner] = useState([])
 
@@ -16,6 +19,14 @@ export function Board(props) {
         width: '30px',
         height: '30px',
     }
+    useEffect(() => {
+        if (!gameOver && board.includes('') && !player.current){
+            setTimeout(() => {
+                computerTurn()
+            }, 500)
+            player.current = true
+        }
+    }, [board])
 
     const Cross = () =>{
         return <img src={crossSrc} style={style} alt='Cross'/>
@@ -27,39 +38,78 @@ export function Board(props) {
 
     const massages = props.messages
 
-    const change = (index) => {
+    function calculateWinner(newBoard) {
+        const winner = CalculateWinner(newBoard)
+        if (winner) {
+            setIsWinner(winner.line)
+            if ((props.you === 1 && winner.winnerSymbol === 'X') ||
+                (props.you === 2 && winner.winnerSymbol === '0')) {
+                setIsLoser(true)
+                props.handleMessages(massages.lose)
+            } else {
+                props.handleMessages(massages.winn)
+                setIsLoser(false)
+            }
+            setGameOver(true)
+            if (winner.winnerSymbol === 'X'){
+                scoreX.current += 1
+            } else if (winner.winnerSymbol === '0'){
+                score0.current += 1
+            }
+            return
+        }
+        if (!newBoard.includes('')) {
+            props.handleMessages(props.messages.tie);
+            setGameOver(true);
+        }
+    }
+
+    const playerTurn = (index) => {
         setBoard(prevBoard => {
             if (prevBoard[index] !== '') {
                 props.handleMessages(props.messages.taken);
                 return prevBoard;
             }
             const newBoard = [...prevBoard];
-            newBoard[index] = xTurn ? 'X' : '0' ;
-            const winner = CalculateWinner(newBoard)
-            if (winner) {
-                setIsWinner(winner.line)
-                if ((props.you === 1 && winner.winnerSymbol === 'X') ||
-                    (props.you === 2 && winner.winnerSymbol === '0')) {
-                    setIsLoser(true)
-                    props.handleMessages(massages.lose)
-                } else {
-                    props.handleMessages(massages.winn)
-                    setIsLoser(false)
-                }
-                setGameOver(true)
-            } else if (!newBoard.includes('')) {
-                props.handleMessages(props.messages.tie);
-                setGameOver(true);
+            if (props.you === 1){
+                newBoard[index] = '0';
+            } else {
+                newBoard[index] = 'X'
             }
+            calculateWinner(newBoard)
             return newBoard;
         });
-        setXTurn(prev => !prev);
-    };
+        console.log(turn)
+        player.current = false
+    }
+
+    const computerTurn = () => {
+        setBoard(prevBoard => {
+            const emptyCells = prevBoard
+                .map((value, index) => (value === '' ? index : null))
+                .filter(index => index !== null)
+            if (emptyCells.length === 0) {
+                setGameOver(true)
+                return prevBoard
+            }
+            const index = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+            const newBoard = [...prevBoard]
+            if (props.you === 1){
+                newBoard[index] = 'X'
+            } else {
+                newBoard[index] = '0'
+            }
+            console.log(newBoard)
+            calculateWinner(newBoard)
+            player.current = true
+            return newBoard
+        })
+    }
 
     return (
         <>
             <h2 className='player'>YOU ARE : {props.you === 1 ? <Circle/> : <Cross/>}</h2>
-            <h2 className='turn'>IT'S TURN: {xTurn ? <Cross /> : <Circle />}</h2>
+            <h2 className='turn'>IT'S TURN: {turn.current ? <Cross/> : <Circle/>}</h2>
             <div className='game'>
                 {board.map((value,index) => (
                     <button
@@ -69,7 +119,7 @@ export function Board(props) {
                                 ? (isLoser ? '#cd1b09' : '#20ff07')
                                 : '#979797'
                         }}
-                        onClick={() => change(index)}
+                        onClick={() => playerTurn(index)}
                         disabled={gameOver}
                     >
                         {value === 'X' && <Cross/>}
@@ -78,12 +128,15 @@ export function Board(props) {
                 ))}
             </div>
             <div>
-                {gameOver && <Attributes
+                <Attributes
                     setBoard={setBoard}
                     setIsWinner={setIsWinner}
+                    gameOver={gameOver}
                     setGameOver={setGameOver}
+                    messages={massages}
                     handleMessages={props.handleMessages}
-                />}
+                    scoreX={scoreX}
+                    score0={score0}/>
             </div>
         </>
     )
