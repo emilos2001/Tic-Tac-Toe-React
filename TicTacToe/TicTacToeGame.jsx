89c7b45ce48676005d53b220b14a16ec9e2fe6d1
Emil Bogaci <140043844+emilos2001@users.noreply.github.com>
2025-08-29 19:26:55 +0300

    Update TicTacToeGame.jsx
    Update

--- TicTacToe/TicTacToeGame.jsx
+++ TicTacToe/TicTacToeGame.jsx
@@ -1,65 +1,143 @@
 import './style.css'
-import {Board} from './Board.jsx'
-import {useState} from "react";
+import {Attributes} from './Attributes.jsx'
+import {useState, useRef, useEffect} from 'react'
+import {CalculateWinner} from './CalculateWinner.js'
 import circleSrc from './Assets/circle.png'
 import crossSrc from './Assets/cross.png'
-export function TicTacToe() {
-    const [gameStart, setGameStart] = useState(false)
-    const [message, setMessage] = useState('')
-    const [you, setYou] = useState(null)
 
-    const Circle =()=>{
-        return <img src={circleSrc} alt="Circle"/>
+export function Board(props) {
+    const player = useRef(true)
+    const scoreX = useRef(0)
+    const score0 = useRef(0)
+    const turn = useRef(true)
+    const [board, setBoard] = useState(Array(9).fill(''))
+    const [gameOver, setGameOver] = useState(false)
+    const [isLoser, setIsLoser] = useState(false)
+    const [isWinner, setIsWinner] = useState([])
+
+    const style = {
+        width: '30px',
+        height: '30px',
     }
-
-    const Cross = () => {
-        return <img src={crossSrc} alt="Cross"/>
-    }
-
-    const handleChoosePlayer = (id) =>{
-        setGameStart(true)
-        setYou(id)
-    }
-
-    const messages = {
-        tie: "IT'S TIE",
-        taken: <h2 className='taken-position'>
-            IT'S ALLREADY TAKEN
-        </h2>,
-        winn: "YOU WON",
-        lose: "YOU LOSE"
-    }
-
-    function handleMesssages(message) {
-        setMessage(message)
-        if (message === messages.taken){
+    useEffect(() => {
+        if (!gameOver && board.includes('') && !player.current){
             setTimeout(() => {
-                setMessage('')
-            }, 4500);
+                computerTurn()
+            }, 500)
+            player.current = true
+        }
+    }, [board])
+
+    const Cross = () =>{
+        return <img src={crossSrc} style={style} alt='Cross'/>
+    }
+
+    const Circle = () =>{
+        return <img src={circleSrc} style={style} alt='Circle'/>
+    }
+
+    const massages = props.messages
+
+    function calculateWinner(newBoard) {
+        const winner = CalculateWinner(newBoard)
+        if (winner) {
+            setIsWinner(winner.line)
+            if ((props.you === 1 && winner.winnerSymbol === 'X') ||
+                (props.you === 2 && winner.winnerSymbol === '0')) {
+                setIsLoser(true)
+                props.handleMessages(massages.lose)
+            } else {
+                props.handleMessages(massages.winn)
+                setIsLoser(false)
+            }
+            setGameOver(true)
+            if (winner.winnerSymbol === 'X'){
+                scoreX.current += 1
+            } else if (winner.winnerSymbol === '0'){
+                score0.current += 1
+            }
+            return
+        }
+        if (!newBoard.includes('')) {
+            props.handleMessages(props.messages.tie);
+            setGameOver(true);
         }
     }
 
-    return(
-        <>
-            <div className='title'>
-                <h2>Tic Tac Toe</h2>
-            </div>
-            {gameStart ?
-                (<Board handleMessages={handleMesssages}
-                        messages={messages}
-                        you={you}
-                        circle={<Circle/>}
-                        cross={<Cross/>}/>
-                )
-                :
-                (
-                    <div className='game-menu'>
-                        <button onClick={() => {handleChoosePlayer(1)}}><Circle/></button>
-                        <button onClick={() => {handleChoosePlayer(2)}}><Cross/></button>
-                    </div>
-                )
+    const playerTurn = (index) => {
+        setBoard(prevBoard => {
+            if (prevBoard[index] !== '') {
+                props.handleMessages(props.messages.taken);
+                return prevBoard;
             }
-            <h2 className='messages'>{message}</h2>
+            const newBoard = [...prevBoard];
+            if (props.you === 1){
+                newBoard[index] = '0';
+            } else {
+                newBoard[index] = 'X'
+            }
+            calculateWinner(newBoard)
+            return newBoard;
+        });
+        console.log(turn)
+        player.current = false
+    }
+
+    const computerTurn = () => {
+        setBoard(prevBoard => {
+            const emptyCells = prevBoard
+                .map((value, index) => (value === '' ? index : null))
+                .filter(index => index !== null)
+            if (emptyCells.length === 0) {
+                setGameOver(true)
+                return prevBoard
+            }
+            const index = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+            const newBoard = [...prevBoard]
+            if (props.you === 1){
+                newBoard[index] = 'X'
+            } else {
+                newBoard[index] = '0'
+            }
+            console.log(newBoard)
+            calculateWinner(newBoard)
+            player.current = true
+            return newBoard
+        })
+    }
+
+    return (
+        <>
+            <h2 className='player'>YOU ARE : {props.you === 1 ? <Circle/> : <Cross/>}</h2>
+            <h2 className='turn'>IT'S TURN: {turn.current ? <Cross/> : <Circle/>}</h2>
+            <div className='game'>
+                {board.map((value,index) => (
+                    <button
+                        key={index}
+                        style={{
+                            backgroundColor: isWinner.includes(index)
+                                ? (isLoser ? '#cd1b09' : '#20ff07')
+                                : '#979797'
+                        }}
+                        onClick={() => playerTurn(index)}
+                        disabled={gameOver}
+                    >
+                        {value === 'X' && <Cross/>}
+                        {value === '0' && <Circle/>}
+                    </button>
+                ))}
+            </div>
+            <div>
+                <Attributes
+                    setBoard={setBoard}
+                    setIsWinner={setIsWinner}
+                    gameOver={gameOver}
+                    setGameOver={setGameOver}
+                    messages={massages}
+                    handleMessages={props.handleMessages}
+                    scoreX={scoreX}
+                    score0={score0}/>
+            </div>
         </>
     )
 }
